4f0cf3374d28b6ea33c2fb9458b02ddd54663948
Lance McDonald <lance.mcdonald@vitals.com>
2017-02-26 16:07:41 -0500

    ProviderQuality kata/code test created and adapted from original VC tests.  Based on an existing CSharp kata found on github

---
+++ src/ProviderQuality.Console/Program.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+
+namespace ProviderQuality.Console
+{
+    public class Program
+    {
+        public IList<Award> Awards
+        {
+            get;
+            set;
+        }
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Updating award metrics...!");
+
+            var app = new Program()
+            {
+                Awards = new List<Award>
+                {
+                    new Award {Name = "Gov Quality Plus", SellIn = 10, Quality = 20},
+                    new Award {Name = "Blue First", SellIn = 2, Quality = 0},
+                    new Award {Name = "ACME Partner Facility", SellIn = 5, Quality = 7},
+                    new Award {Name = "Blue Distinction Plus", SellIn = 0, Quality = 80},
+                    new Award {Name = "Blue Compare", SellIn = 15, Quality = 20},
+                    new Award {Name = "Top Connected Providres", SellIn = 3, Quality = 6}
+                }
+
+            };
+
+            app.UpdateQuality();
+
+            System.Console.ReadKey();
+
+        }
+
+        public void UpdateQuality()
+        {
+            for (var i = 0; i < Awards.Count; i++)
+            {
+                if (Awards[i].Name != "Blue First" && Awards[i].Name != "Blue Compare")
+                {
+                    if (Awards[i].Quality > 0)
+                    {
+                        if (Awards[i].Name != "Blue Distinction Plus")
+                        {
+                            Awards[i].Quality = Awards[i].Quality - 1;
+                        }
+                    }
+                }
+                else
+                {
+                    if (Awards[i].Quality < 50)
+                    {
+                        Awards[i].Quality = Awards[i].Quality + 1;
+
+                        if (Awards[i].Name == "Blue Compare")
+                        {
+                            if (Awards[i].SellIn < 11)
+                            {
+                                if (Awards[i].Quality < 50)
+                                {
+                                    Awards[i].Quality = Awards[i].Quality + 1;
+                                }
+                            }
+
+                            if (Awards[i].SellIn < 6)
+                            {
+                                if (Awards[i].Quality < 50)
+                                {
+                                    Awards[i].Quality = Awards[i].Quality + 1;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (Awards[i].Name != "Blue Distinction Plus")
+                {
+                    Awards[i].SellIn = Awards[i].SellIn - 1;
+                }
+
+                if (Awards[i].SellIn < 0)
+                {
+                    if (Awards[i].Name != "Blue First")
+                    {
+                        if (Awards[i].Name != "Blue Compare")
+                        {
+                            if (Awards[i].Quality > 0)
+                            {
+                                if (Awards[i].Name != "Blue Distinction Plus")
+                                {
+                                    Awards[i].Quality = Awards[i].Quality - 1;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Awards[i].Quality = Awards[i].Quality - Awards[i].Quality;
+                        }
+                    }
+                    else
+                    {
+                        if (Awards[i].Quality < 50)
+                        {
+                            Awards[i].Quality = Awards[i].Quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+}
---
+++ src/ProviderQuality.Tests/UpdateQualityAwardsTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProviderQuality.Console;
+
+namespace ProviderQuality.Tests
+{
+    [TestClass]
+    public class UpdateQualityAwardsTests
+    {
+        [TestMethod]
+        public void TestImmutabilityOfBlueDistinctionPlus()
+        {
+            var app = new Program()
+            {
+                Awards = new List<Award>
+                {
+                    new Award {Name = "Blue Distinction Plus", SellIn = 0, Quality = 80}
+                }
+            };
+
+            Assert.IsTrue(app.Awards.Count == 1);
+            Assert.IsTrue(app.Awards[0].Name == "Blue Distinction Plus");
+            Assert.IsTrue(app.Awards[0].Quality == 80);
+
+            app.UpdateQuality();
+
+            Assert.IsTrue(app.Awards[0].Quality == 80);
+        }
+
+        // +++To Do - 1/10/2013: Discuss with team about adding more tests.  Seems like a lot of work for something
+        //                       that probably won't change.  I watched it all in the debugger and know everything works
+        //                       plus QA has already signed off and no one has complained.
+    }
+}
